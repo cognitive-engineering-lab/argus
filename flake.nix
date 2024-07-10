4a234fde3e929b22c8903f950f29e5767db2e6e9
gavinleroy <gavinleroy6@gmail.com>
2024-07-10 14:00:11 -0400

    Update Flake

--- flake.nix
+++ flake.nix
@@ -12,7 +12,33 @@
     pkgs = import nixpkgs {
       inherit system overlays;
     };
+
     toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    depotjs = pkgs.rustPlatform.buildRustPackage rec {
+      pname = "depot";
+      version = "0.2.17";
+      src = pkgs.fetchFromGitHub {
+        owner = "cognitive-engineering-lab";
+        repo = pname;
+        rev = "v${version}";        
+        hash = "sha256-kiQXxTVvzfovCn0YmOH/vTUQHyRS39gH7iBGaKyRZFg=";
+      };
+
+      cargoHash = "sha256-m9sG//vBUqGLwWHkyq+sJ8rkQOeaif56l394dgPU1uQ=";
+
+      # Depot tests require lots of external toolchains, node, typedoc, biome, ...
+      # so we'll just skip all tests for now and figure this out later.
+      doCheck = false;
+
+      nativeBuildInputs = with pkgs; [ pkg-config ];
+
+      buildInputs = with pkgs; [
+        bzip2
+        openssl
+      ] ++ lib.optionals stdenv.isDarwin [
+        darwin.apple_sdk.frameworks.SystemConfiguration
+      ];    
+    };
   in {
     devShell = with pkgs; mkShell { 
       buildInputs = [ 
@@ -22,6 +48,9 @@
         guile
         nodejs_22
         nodePackages.pnpm
+        depotjs
+        cargo-make
+        cargo-watch
         toolchain
       ] ++ lib.optional stdenv.isDarwin libiconv; 
 
